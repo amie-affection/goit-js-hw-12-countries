aad59c6773a3497521b3caf20cbfe44be240e4a3
amie-affection <amie.affection@gmail.com>
2020-05-21 22:56:24 +0300

    modified files

--- src/countries.js
+++ src/countries.js
@@ -1,6 +1,22 @@
-const basisUrl = 'https://restcountries.eu/rest/v2/name';
+const basisUrl = 'https://restcountries.eu/rest/v2/name/';
 
 export const fetchCountries = inputValue => {
-    fetch(`${basisUrl}${inputValue}`)
-    .then(response => {return response.json})
+  fetch(`${basisUrl}${inputValue}`)
+    .then(response => {
+      if (response.status < 400) {
+        return response.json();
+      }
+    })
+    .then(data => {
+      if (data.length > 10) {
+        console.log('много совпадений');
+      }
+      if (data.length < 10 && data.length > 2) {
+        console.log('список объектов');
+        }
+        if (data.length === 1) {
+            console.log('1 объект');
+        }
+      console.log(data);
+    });
 };
--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import './styles.css';
 import { fetchCountries } from './countries.js';
-import debounce from 'lodash';
+import lodash from 'lodash';
 
 const input = document.querySelector('.searchInput');
+// console.log(input);
 input.addEventListener(
   'input',
-  debounce(() => {
+  lodash.debounce(() => {
     fetchCountries(input.value);
   }, 300),
 );
